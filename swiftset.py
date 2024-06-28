42576b370f27d979aab73098459d4bb6d7007424
archonxk <172537115+archonxk@users.noreply.github.com>
2024-06-29 02:32:45 +0530

    Arg parsing options
    Written and test by: GOC

--- swiftset.py
+++ swiftset.py
@@ -1,5 +1,15 @@
 import os
+from core.nmap import *
+import sys
+import argparse
 
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+clear_screen()
 print(
     """
    ____       _ _____    ____    __ 
@@ -10,4 +20,17 @@ print(
     """
 )
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', help='URL / Domain you ant to scan')
+parser.add_argument('--version', help='View the version of Swift Set')
+parser.add_argument('--view_scans', help='View all scans currently installed')
+parser.add_argument('--view_core', help='View all core scans currently installed')
+parser.add_argument('--headers', help='Specify headers file for all requests')
+parser.add_argument('--cookies', help='Specify cookies file for all requests')
+parser.add_argument('--all_scans', help='Run all available scans')
+parser.add_argument('--core_scans', help='Run all available core scans')
+parser.add_argument('--specific_scans', help='Run some specific scans (comma seperated)')
+
+
+args = parser.parse_args()
 
